cfaf063a570a8497bfcb3aed7b4668255d31c790
Jasper Blaauw <0913442@hr.nl>
2016-10-03 23:32:48 +0200

    Damspel zonder GUI of AI
    run main in damspel.java, voer opdrachten in om te spelen.

--- code/Dambord.java
+++ code/Dambord.java
@@ -25,9 +25,11 @@ public class Dambord {
 	private static final int ZWARTEDAM = 3;
 	private static final int WITTEDAM = 4;
 	
-	//Wit begint het spel altijd.
+	//Wit begint altijd met de eerste zet.
 	private int speler = WIT;
 	private int tegenstander = ZWART;
+	private int aantalZwarteStenen = 20;
+	private int aantalWitteStenen = 20;
 	
 	//################################# METHODS #####################################
 	
@@ -41,29 +43,95 @@ public class Dambord {
 		}
 	}
 	
-	public void schuif(String richting){
-		
-		//schuif steen 1 plaats naar boven en 1 plaats naar links
-		if(richting.equals("links") && geselecteerd[0] != 0 && geselecteerd[1] != 0){
-			//als er geen steen links-boven ligt:
-			if(stenen[geselecteerd[0]-1][geselecteerd[1]-1] == LEEG){
-					int temp = stenen[geselecteerd[0]][geselecteerd[1]];
-					stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
-					stenen[geselecteerd[0]-1][geselecteerd[1]-1] = temp;
-			}
-		}
+	public int getGeselecteerd(){
+		return stenen[geselecteerd[0]][geselecteerd[1]];
+	}
 	
-		//schuif steen 1 plaats naar boven en 1 plaats naar rechts
-		else if(richting.equals("rechts") && geselecteerd[0] != 0 && geselecteerd[1] != 9){
-			//als er geen steen rechts-boven ligt:
-			if(stenen[geselecteerd[0]-1][geselecteerd[1]+1] == LEEG){
-				int temp = stenen[geselecteerd[0]][geselecteerd[1]];
-				stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
-				stenen[geselecteerd[0]-1][geselecteerd[1]+1] = temp;
+	public void printScore(){
+		if(aantalWitteStenen == 0)
+			System.out.println("Zwart heeft gewonnen.");
+		else if(aantalZwarteStenen == 0)
+			System.out.println("Wit heeft gewonnen.");
+		else{
+			System.out.println("Wit heeft " + aantalWitteStenen + " stenen over. Zwart heeft " + aantalZwarteStenen + " stenen over.");
+		}
+	}
+	
+	public boolean schuif(String richting){
+		if(!kanSlaan()){
+			//schuif steen 1 plaats naar boven en 1 plaats naar links
+			if(richting.equals("linksboven") && geselecteerd[0] != 0 && geselecteerd[1] != 0){
+				//als er geen steen links-boven ligt:
+				if(stenen[geselecteerd[0]-1][geselecteerd[1]-1] == LEEG){
+						int temp = getGeselecteerd();
+						stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+						stenen[geselecteerd[0]-1][geselecteerd[1]-1] = temp;
+						//als de steen naar de rand van de tegenstander is geschoven, wordt het een dam.
+						if(geselecteerd[0]-1 == 0){
+							stenen[geselecteerd[0]-1][geselecteerd[1]-1] += 2;
+						}
+						return true;
+				}
+				else{
+					System.out.println("Deze steen kan hier niet heen schuiven.");
+					return false;
+				}
+			}
+		
+			//schuif steen 1 plaats naar boven en 1 plaats naar rechts
+			else if(richting.equals("rechtsboven") && geselecteerd[0] != 0 && geselecteerd[1] != 9){
+				//als er geen steen rechts-boven ligt:
+				if(stenen[geselecteerd[0]-1][geselecteerd[1]+1] == LEEG){
+					int temp = getGeselecteerd();
+					stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+					stenen[geselecteerd[0]-1][geselecteerd[1]+1] = temp;
+					//als de steen naar de rand van de tegenstander is geschoven, wordt het een dam.
+					if(geselecteerd[0]-1 == 0){
+						stenen[geselecteerd[0]-1][geselecteerd[1]-1] += 2;
+					}
+					return true;
+				}
+				else{
+					System.out.println("Deze steen kan hier niet heen schuiven.");
+					return false;
+				}
+			}
+			//schuif steen 1 plaats naar links en 1 plaats naar onder. Alleen bij een dam.
+			else if(richting.equals("linksonder") && geselecteerd[0] != 9 && geselecteerd[1] != 0 && (getGeselecteerd() == WITTEDAM || getGeselecteerd() == ZWARTEDAM)){
+				//als er geen steen links-onder ligt:
+				if(stenen[geselecteerd[0]+1][geselecteerd[1]-1] == LEEG){
+					int temp = getGeselecteerd();
+					stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+					stenen[geselecteerd[0]+1][geselecteerd[1]-1] = temp;
+					return true;
+				}
+				else{
+					System.out.println("Deze steen kan hier niet heen schuiven.");
+					return false;
+				}
+			}
+			//schuif steen 1 plaats naar rechts en 1 plaats naar onder. Alleen bij een dam.
+			else if(richting.equals("rechtsonder") && geselecteerd[0] != 9 && geselecteerd[1] != 9 && (getGeselecteerd() == WITTEDAM || getGeselecteerd() == ZWARTEDAM)){
+				//als er geen steen rechts-onder ligt:
+				if(stenen[geselecteerd[0]+1][geselecteerd[1]+1] == LEEG){
+					int temp = getGeselecteerd();
+					stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+					stenen[geselecteerd[0]+1][geselecteerd[1]+1] = temp;
+					return true;
+				}
+				else{
+					System.out.println("Deze steen kan hier niet heen schuiven.");
+					return false;
+				}
+			}
+			else{
+				System.out.println("Deze steen kan hier niet heen schuiven.");
+				return false;
 			}
 		}
 		else{
-			System.out.println("Deze steen kan hier niet heen schuiven.");
+			System.out.println("U kan niet schuiven omdat er geslagen kan worden.");
+			return false;
 		}
 	}
 	
@@ -72,9 +140,9 @@ public class Dambord {
 		
 		for(int x = 0; x <= 9; x++){
 			for(int y = 0; y <= 9; y++){
-				if(stenen[x][y] == speler && x != 0){
+				if(stenen[x][y] == speler ){
 					//check links-boven
-					if(y != 0){
+					if(x != 0 && y != 0){
 						if(stenen[x-1][y-1] == tegenstander && x-1 != 0 && y-1 != 0){
 							if(stenen[x-2][y-2] == LEEG){
 								return true;
@@ -82,9 +150,25 @@ public class Dambord {
 						}
 					}
 					//check rechts-boven
-					if(y != 9){
-						if(stenen[x-1][y+1] == tegenstander && x-1 != 0 && y+1 != 0){
+					if(x != 0 && y != 9){
+						if(stenen[x-1][y+1] == tegenstander && x-1 != 0 && y+1 != 9){
 							if(stenen[x-2][y+2] == LEEG){
+								return true;
+							}
+						}
+					}
+					//check links-onder
+					if(x != 9 && y != 0){
+						if(stenen[x+1][y-1] == tegenstander && x+1 != 9 && y-1 != 0){
+							if(stenen[x+2][y-2] == LEEG){
+								return true;
+							}
+						}
+					}
+					//check rechts-onder
+					if(x != 9 && y != 9){
+						if(stenen[x+1][y+1] == tegenstander && x+1 != 9 && y+1 != 9){
+							if(stenen[x+2][y+2] == LEEG){
 								return true;
 							}
 						}
@@ -93,6 +177,97 @@ public class Dambord {
 			}
 		}
 		return false;
+	}
+	
+	public boolean sla(String richting){
+		if(kanSlaan()){
+			switch(richting){
+			case "linksboven": 
+				if(geselecteerd[0] != 0 && geselecteerd[1] != 0){
+					if(stenen[geselecteerd[0]-1][geselecteerd[1]-1] == tegenstander && geselecteerd[0]-1 != 0 && geselecteerd[1]-1 != 0){
+						if(stenen[geselecteerd[0]-2][geselecteerd[1]-2] == LEEG){
+							if(getGeselecteerd() == WIT)
+								aantalZwarteStenen--;
+							if(getGeselecteerd() == ZWART)
+								aantalWitteStenen--;
+							stenen[geselecteerd[0]-2][geselecteerd[1]-2] = getGeselecteerd();
+							stenen[geselecteerd[0]-1][geselecteerd[1]-1] = LEEG;
+							stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+							return true;
+						}
+					}
+				}
+				break;
+			case "rechtsboven":
+				if(geselecteerd[0] != 0 && geselecteerd[1] != 9){
+					if(stenen[geselecteerd[0]-1][geselecteerd[1]+1] == tegenstander && geselecteerd[0]-1 != 0 && geselecteerd[1]+1 != 9){
+						if(stenen[geselecteerd[0]-2][geselecteerd[1]+2] == LEEG){
+							if(getGeselecteerd() == WIT)
+								aantalZwarteStenen--;
+							if(getGeselecteerd() == ZWART)
+								aantalWitteStenen--;
+							stenen[geselecteerd[0]-2][geselecteerd[1]+2] = getGeselecteerd();
+							stenen[geselecteerd[0]-1][geselecteerd[1]+1] = LEEG;
+							stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+							return true;
+						}
+					}
+				}
+				break;
+			case "linksonder":
+				if(geselecteerd[0] != 9 && geselecteerd[1] != 0){
+					if(stenen[geselecteerd[0]+1][geselecteerd[1]-1] == tegenstander && geselecteerd[0]+1 != 9 && geselecteerd[1]-1 != 0){
+						if(stenen[geselecteerd[0]+2][geselecteerd[1]-2] == LEEG){
+							if(getGeselecteerd() == WIT)
+								aantalZwarteStenen--;
+							if(getGeselecteerd() == ZWART)
+								aantalWitteStenen--;
+							stenen[geselecteerd[0]+2][geselecteerd[1]-2] = getGeselecteerd();
+							stenen[geselecteerd[0]+1][geselecteerd[1]-1] = LEEG;
+							stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+							return true;
+						}
+					}
+				}
+				break;
+			case "rechtsonder":
+				if(geselecteerd[0] != 9 && geselecteerd[1] != 9){
+					if(stenen[geselecteerd[0]+1][geselecteerd[1]+1] == tegenstander && geselecteerd[0]+1 != 9 && geselecteerd[1]+1 != 9){
+						if(stenen[geselecteerd[0]+2][geselecteerd[1]+2] == LEEG){
+							if(getGeselecteerd() == WIT)
+								aantalZwarteStenen--;
+							if(getGeselecteerd() == ZWART)
+								aantalWitteStenen--;
+							stenen[geselecteerd[0]+2][geselecteerd[1]+2] = getGeselecteerd();
+							stenen[geselecteerd[0]+1][geselecteerd[1]+1] = LEEG;
+							stenen[geselecteerd[0]][geselecteerd[1]] = LEEG;
+							return true;
+						}
+					}
+				}
+				break;
+			default: break;
+			}
+		}
+		else{
+			System.out.println("Er kan niet geslagen worden.");
+			return false;
+		}
+		return false;
+	}
+	
+	public void draaiBord(){
+		int stenen2[][] = new int[10][10];
+		for(int x = 0; x <= 9; x++){
+			for(int y = 0; y <= 9; y++){
+				stenen2[x][y] = stenen[9-x][9-y];
+			}
+		}
+		stenen = stenen2;
+		//beurt voorbij, dus wissel speler en tegenstander
+		int temp = speler;
+		speler = tegenstander;
+		tegenstander = temp;
 	}
 	
 	public void printBord(){
--- code/Damspel.java
+++ code/Damspel.java
@@ -1,20 +1,97 @@
+import java.util.Scanner;
+
 
 public class Damspel {
 	
+	public static void beëindigBeurt(Dambord bord){
+		System.out.println("Na uw opdracht ziet het bord er zo uit:");
+		bord.printBord();
+		System.out.println("De volgende speler is aan de beurt. Van zijn/haar perspectief ziet het bord er zo uit:");
+		bord.draaiBord();
+		bord.printBord();
+		System.out.println("Welke opdracht wilt u uitvoeren?");
+	}
+	
 	public static void main(String[] args){
 		Dambord bord = new Dambord();
-		
-		
-		// Slaan- en schuiftest
+		/*
+		// Slaan- en schuiftesten
 		bord.setGeselecteerd(6, 1);
-		bord.schuif("links");
+		bord.schuif("linksboven");
+		bord.printBord();
+		System.out.println(bord.kanSlaan() ? "Er kan geslagen worden" : "Er kan niet geslagen worden");
+		System.out.println();
+		
+		bord.draaiBord();
+		System.out.println(bord.kanSlaan() ? "Er kan geslagen worden" : "Er kan niet geslagen worden");
+		System.out.println();
+		
+		bord.setGeselecteerd(6, 7);
+		bord.schuif("rechtsboven");
 		bord.printBord();
 		
+		System.out.println(bord.kanSlaan() ? "Er kan geslagen worden" : "Er kan niet geslagen worden");
+		System.out.println();
+		bord.draaiBord();
+		System.out.println(bord.kanSlaan() ? "Er kan geslagen worden" : "Er kan niet geslagen worden");
 		System.out.println();
 		
 		bord.setGeselecteerd(5, 0);
-		bord.schuif("rechts");
+		bord.sla("rechtsboven");
 		bord.printBord();
+		System.out.println(bord.kanSlaan() ? "Er kan geslagen worden" : "Er kan niet geslagen worden");
+		
+		bord.printScore();
+		*/
+		
+		
+		
+		boolean flag = false;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welkom bij dammen met CheckerMate!");
+		System.out.println("Het bord ziet er zo uit:");
+		bord.printBord();
+		System.out.println("Legenda: 0 = een leeg veld, 1 = een zwarte steen, 2 = een witte steen, 3 = een zwarte dam, 4 = een witte dam.");
+		System.out.println("Geldige opdrachten zijn:\n- exit/stop/afsluiten\n- selecteer x y\n- schuif {linksboven, linksonder, rechtsboven, rechtsonder}\n- sla {linksboven, linksonder, rechtsboven, rechtsonder}\n- draai/draaien/draai bord\n- score\nWelke opdracht wilt u nu uitvoeren?");
+		
+        while(sc.hasNextLine() && !flag){
+        	
+        	String input = sc.nextLine();
+        	if(input.equals("exit") || input.equals("stop") || input.equals("afsluiten")){
+        		sc.close();
+        		System.exit(0);
+        	}
+        	if(input.equals("help")){
+        		System.out.println("Geldige opdrachten zijn:\n- exit/stop/afsluiten\n- selecteer x y\n- schuif {linksboven, linksonder, rechtsboven, rechtsonder}\n- sla {linksboven, linksonder, rechtsboven, rechtsonder}\n- draai/draaien/draai bord\n- score\nWelke opdracht wilt u nu uitvoeren?");
+        	}
+        	else if(input.startsWith("selecteer")){
+        		if(Character.getNumericValue(input.charAt(10)) >= 0 && Character.getNumericValue(input.charAt(10)) <= 9 && Character.getNumericValue(input.charAt(12)) >= 0 && Character.getNumericValue(input.charAt(12)) <= 9){
+        			bord.setGeselecteerd(Character.getNumericValue(input.charAt(10)), Character.getNumericValue(input.charAt(12)));
+        			System.out.println("U selecteerde: " + input.charAt(10) + ", " + input.charAt(12) + ".");
+        		}
+        		System.out.println("Welke opdracht wilt u nu uitvoeren?");
+        	}
+        	else if(input.startsWith("schuif")){
+        		if(bord.schuif(input.substring(7, input.length())))
+        			beëindigBeurt(bord);
+        	}
+        	else if(input.startsWith("sla")){
+        		if(bord.sla(input.substring(4, input.length())))
+        			beëindigBeurt(bord);
+        	}
+        	else if(input.equals("draai") || input.equals("draaien") || input.equals("draai bord")){
+        		bord.draaiBord();
+        		beëindigBeurt(bord);
+        	}
+        	else if(input.equals("score")){
+        		bord.printScore();
+        		System.out.println("Welke opdracht wilt u nu uitvoeren?");
+        	}
+        	else{
+        		System.out.println("Dit is geen geldige invoer.\nVoer een andere opdracht uit.\nTyp help als u alle mogelijke opdrachten wilt bekijken.");
+        	}
+        }
+        sc.close();
 	}
-
+	
 }
