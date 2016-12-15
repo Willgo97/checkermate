8b5946f6915d0e64176f340967c555ce4ae3b55d
Willgo97 <williamgoudswaard@gmail.com>
2016-12-15 17:08:41 +0100

    time for food :)

--- code/arduino/robotarmv4/robotarmv4.ino
+++ code/arduino/robotarmv4/robotarmv4.ino
@@ -1,3 +1,5 @@
+#include <CheckBoard.h>
+
 const int stepPinY = 3; // Y axis
 const int dirPinY = 4;
 const int stepPinX = 5; // X axis
@@ -5,9 +7,9 @@ const int dirPinX = 6;
 
 const int magnet = 12;//magnet connected to MOSFET
 
-const int rows[5] = {23, 25, 27, 29, 31};// checkerboard
-const int columns[10] = {33, 35, 37, 39, 41, 43, 45, 47, 49, 51};
-int array[10][10];
+int rows[5] = {23, 25, 27, 29, 31};// checkerboard
+int columns[10] = {33, 35, 37, 39, 41, 43, 45, 47, 49, 51};
+CheckBoard checkBoard(rows, columns);
 
 const int magnetMotorPin1 = 46;// z axis
 const int magnetMotorPin2 = 44;
@@ -40,17 +42,6 @@ void setup() {
   pinMode(limitsZ, INPUT);
 
   pinMode(magnet, OUTPUT);
-
-  // Set the pins for the checkerboard
-  for (int i = 0; i < 10; i++) {
-    pinMode(columns[i], OUTPUT);
-    digitalWrite(columns[i], LOW);
-  }
-
-  for (int i = 0; i < 5; i++) {
-    pinMode(rows[i], INPUT);
-  }
-
   Serial.begin(9600);
 }
 
@@ -93,8 +84,8 @@ void loop() {
         break;
       case 'b' :
         Serial.println("printing board layout");
-        checkBoard();
-        printBoard();
+        checkBoard.checkB();
+        checkBoard.printB();
         break;
     }
   }
@@ -298,46 +289,4 @@ void SetDirection() {
   if (Steps < 0) {
     Steps = 7;
   }
-}
-
-void checkB() { // This reads the sensors under the board to determine the position of all the pieces
-  int foo = 0;
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(columns[i], HIGH);
-    delay(2);
-    for (int j = 0; j < 5; j++) {
-      delay(2);
-      //Serial.print(digitalRead(rows[j]));
-      if (i % 2 == 0 ) {
-        foo = 1;
-      } else {
-        foo = 0;
-      }
-      if (digitalRead(rows[j])) {
-        array[(j * 2) + foo][i] = 0;
-      } else {
-        array[(j * 2) + foo][i] = 1;
-      }
-    }
-    digitalWrite(columns[i], LOW);
-    delay(2);
-    //Serial.println();
-  }
-}
-
-void printB() {
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      if (array[i][j]) {
-        Serial.print("1");
-        //Serial.print("[X]");
-      } else {
-        Serial.print("0");
-        //Serial.print("[ ]");
-      }
-    }
-    Serial.print(",");
-    //Serial.println();
-  }
-  Serial.println();
 }
